d5037b49b9d968689e60d8e119e30a3134fe3361
liujianjiang <875394153@qq.com>
2017-07-07 16:55:50 +0800

    vueadmin

--- resources/assets/js/views/dashboard/article/Create.vue
+++ resources/assets/js/views/dashboard/article/Create.vue
@@ -1,80 +1,33 @@
 <template>
-    <el-form ref="myForm" :model="myForm" :rules="myFormRules" label-width="100px" style="margin:20px;width:60%;min-width:600px;">
-        <el-form-item label="分类" prop="options">
-            <el-select v-model="myForm.category_id" placeholder="请选择分类">
-                <el-option v-for="item in options.data" :key="item.id" :label="item.name" :value="item.id"></el-option>
-            </el-select>
-        </el-form-item>
-        <el-form-item label="标题" prop="title">
-            <el-input v-model="myForm.title" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="短标题" prop="subtitle">
-            <el-input v-model="myForm.subtitle" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="标签" prop="allTag">
-            <el-select v-model="myForm.tag_id" multiple placeholder="请选择">
-                <el-option
-                        v-for="field in allTag"
-                        :key="field.id"
-                        :label="field.tag"
-                        :value="field.id">
-                </el-option>
-            </el-select>
-        </el-form-item>
-        <el-form-item label="描述">
-            <el-input
-                    type="textarea"
-                    :rows="2"
-                    placeholder="请输入内容"
-                    v-model="myForm.meta_description">
-            </el-input>
-        </el-form-item>
-        <el-form-item label="内容">
-            <textarea id="editor"></textarea>
-        </el-form-item>
-        <el-form-item label="时间">
-            <div class="block">
-                <span class="demonstration"></span>
-                <el-date-picker
-                        v-model="myForm.published_at"
-                        type="datetime"
-                        placeholder="选择日期时间">
-                </el-date-picker>
-            </div>
-        </el-form-item>
-        <el-form-item label="">
-            <el-row>
-                <el-col :span="8"><div class="grid-content bg-purple-light">是否存草稿？<el-switch
-                        v-model="myForm.is_draft"
-                        on-color="#13ce66"
-                        off-color="#ff4949">
-                </el-switch></div></el-col>
-                <el-col :span="8"><div class="grid-content bg-purple-light">是否是原创？<el-switch
-                        v-model="myForm.is_original"
-                        on-color="#13ce66"
-                        off-color="#ff4949">
-                </el-switch></div></el-col>
-            </el-row>
-            </el-switch>
-        </el-form-item>
-        <el-form-item>
-            <el-button type="primary" size="large" @click.native="onSubmit('myForm')">保存</el-button>
-        </el-form-item>
-    </el-form>
+    <article-form :myForm="myForm"></article-form>
 </template>
 
 
 <script type="text/ecmascript-6">
-    import FormMixin from './FormMixin.vue'
-    import util from '../../../common/js/util';
-    import {default as SimpleMDE} from 'simplemde/dist/simplemde.min.js';
-    import { error_warning } from '../../../config/helper'
+    import ArticleForm from './Form'
+
     export default {
-        mixins: [FormMixin],
+        components: { ArticleForm },
+        data() {
+            return {
+                myForm: {
+                    category_id: '',
+                    tag_id: [],
+                    title: '',
+                    subtitle: '',
+                    content: '',
+                    meta_description: '',
+                    published_at: new Date(),
+                    is_draft: false,
+                    is_original: false,
+                }
+            }
+        },
+     /*   mixins: [FormMixin],
         data(){
             return {
                 myFormRules: {
-                    /* title: [
+                    /!* title: [
                      {required: true, message: '请输入标题', trigger: 'blur'}
                      ],
                      subtitle: [
@@ -82,7 +35,7 @@
                      ],
                      category_id: [
                      { type: 'number', required: true, message: '请选择', trigger: 'change'}
-                     ]*/
+                     ]*!/
                 },
                 myForm: {
                     category_id: '',
@@ -133,7 +86,7 @@
                 placeholder: 'Please input the article content.',
                 autoDownloadFontAwesome: true
             })
-        },
+        },*/
     }
 </script>
 
